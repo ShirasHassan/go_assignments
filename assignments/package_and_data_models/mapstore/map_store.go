8ddc65386cbb2061a89bbd8d411635a0cb36d552
ShirasHassan <Shiras.Hassan@gmail.com>
2020-11-27 23:02:00 +0530

    revert map_store changes

--- assignments/package_and_data_models/mapstore/map_store.go
+++ assignments/package_and_data_models/mapstore/map_store.go
@@ -2,8 +2,55 @@ package mapstore
 
 import (
 	"assignment1/domain"
+	"errors"
 )
 
 type MapStore struct {
 	store map[string]domain.Customer
+}
+
+func NewMapStore() domain.CustomerStore {
+	return &MapStore{
+		store: make(map[string]domain.Customer)}
+}
+
+func (m *MapStore) Create(customer domain.Customer) error {
+	if _, hasVal := m.store[customer.ID]; hasVal == true {
+		return errors.New("'Customer '" + customer.ID + "' is already exist.'")
+	}
+	m.store[customer.ID] = customer
+	return nil
+}
+
+func (m *MapStore) Update(id string, customer domain.Customer) error {
+	if _, hasVal := m.store[id]; hasVal == false {
+		return errors.New("'Customer '" + id + "' is not exist.'")
+	}
+	customer.ID = id
+	m.store[id] = customer
+	return nil
+}
+
+func (m *MapStore) Delete(id string) error {
+	if _, hasVal := m.store[id]; hasVal == false {
+		return errors.New("'Customer '" + id + "' is not exist.'")
+	}
+	delete(m.store, id)
+	return nil
+}
+
+func (m MapStore) GetAll() ([]domain.Customer, error) {
+	customers := make([]domain.Customer, 0)
+	for _, customer := range m.store {
+		customers = append(customers, customer)
+	}
+	return customers, nil
+}
+
+
+func (m *MapStore) GetById(id string) (domain.Customer, error) {
+	if _, hasVal := m.store[id]; hasVal == false {
+		return domain.Customer{}, errors.New("'Customer '" + id + "' is not exist.'")
+	}
+	return m.store[id], nil
 }
--- assignments/package_and_data_models/mapstore/map_store_factory.go
+++
@@ -1,8 +0,0 @@
-package mapstore
-
-import "assignment1/domain"
-
-func NewMapStore() domain.CustomerStore {
-	return &MapStore{
-		store: make(map[string]domain.Customer)}
-}
